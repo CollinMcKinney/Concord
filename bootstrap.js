45f32fa5e9a258200830c56cf3af77bb4986be7f
Collin McKinney <collinmckinney4@gmail.com>
2026-03-19 11:44:24 -0500

    Pinned Node and Redis images to specific versions for reproducibility.

--- bootstrap.js
+++ bootstrap.js
@@ -1,15 +1,6 @@
 const { execSync } = require('child_process');
 
 try {
-
-
-    console.log('Installing global tools...');
-    execSync('npm install -g yarn', { stdio: 'inherit' });
-    // execSync('yarn global add pm2', { stdio: 'inherit' });
-
-    console.log('Installing dependencies...');
-    execSync('yarn install', { stdio: 'inherit' });
-
     console.log('Starting services...');
     execSync('podman compose --file podman-compose.yaml up -d', { stdio: 'inherit' });
     console.log('All services started successfully!');
